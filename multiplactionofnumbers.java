0343e8c0b504189c91344a7f35c4daeaa84c6d27
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-08-24 21:13:14 +0530

    Add files via upload

---
+++ multiplactionofnumbers.java
@@ -0,0 +1,14 @@
+public # Program to multiply two numbers
+
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Multiply the numbers
+result = num1 * num2
+
+# Display the result
+print("The result of multiplication is:", result)
+ multiplactionofnumbers {
+    
+}
