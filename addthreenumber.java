678a72cfb568241771068352491905de11d7710d
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-08-26 10:58:01 +0530

    Add files via upload

---
+++ addthreenumber.java
@@ -0,0 +1,14 @@
+public class addthreenumber{
+
+    public static void main(String[] args){
+
+        System.out.println("The Sum of the three numbers are:");
+
+        int num1= 49;
+        int num2= 90;
+        int num3= 87;
+        int sum= num1+num2+num3;
+        System.out.println(sum);
+    }
+
+}
