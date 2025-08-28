c03d6df5e800cfdd343bd0ffc6bc4c917f5bc5a7
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-08-28 21:37:00 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,17 @@
+public class first {
+
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = true;
+
+        if(a&&b){
+            System.out.println("Y");
+        }
+        else{
+            System.out.println("N");
+        }
+    }
+
+    
+}
