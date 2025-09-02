677abd7fdb918809ba41c59d9930a2044cf6cd10
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-02 21:21:05 +0530

    Add files via upload

---
+++ agecheck.java
@@ -0,0 +1,14 @@
+public class agecheck{
+    public static void main(String[] args) {
+        
+        int  age=16;
+        if(age>18){
+            System.out.println("You are eligible to drive");
+        }
+            else{
+                System.out.println("You are not eligible to drive");
+            }
+
+        
+    }
+}
