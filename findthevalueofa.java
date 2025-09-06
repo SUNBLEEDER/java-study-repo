3719e9680f28656f41a4fcdfba532e88a3d6a649
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-06 19:56:37 +0530

    Add files via upload

---
+++ findthevalueofa.java
@@ -0,0 +1,9 @@
+public class findthevalueofa {
+    public static void main(String[] args) {
+        
+        int x = 7;
+        int a = 7*49/7+35/7;
+        System.out.println(a);
+    }
+    
+}
