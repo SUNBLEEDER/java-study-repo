590307474afa755ea67c94d823e2e6d2c4001262
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-09 18:42:30 +0530

    Add files via upload

---
+++ spaceinstrings.java
@@ -0,0 +1,10 @@
+public class spaceinstrings {
+    public static void main(String[] args) {
+        
+
+        String name= "Hello everyone      my name        is Rishiraj       Karan";
+        System.out.println(name.indexOf("  "));
+        System.out.println(name.indexOf("   "));
+    }
+    
+}
