17c3109cd583cdab105c53442b60aab4b65317fe
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-10 19:05:03 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,21 @@
+public class test {
+    public static void main(String[] args) {
+        
+       int num = 34;
+       System.out.println("The first number that you have entered is:"  + num);    // in this program the int is not used because it is already declare in the below of them 
+
+       int num2 = 36;
+       System.out.println("The second number that you have entered is:" + num2);    // in this program the int is not used because it is already declare in the below of them 
+
+
+       int num3 = 556;
+       System.out.println("The third number that you have enterd is:" + num3);    // in this program the int is not used because it is already declare in the below of them 
+
+
+        int sum = num+num2+ num3;
+       System.out.println("the three numbers that you have entered are :"  +  num + ", " + num2 + ", " + num3);
+
+       System.out.println("The sum of the three nmbers that you have entered are " + sum);
+    }
+    
+}
