a4d635a66607c98279339141c8c8953a461272e9
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-12 20:10:47 +0530

    Add files via upload

---
+++ usingoperators.java
@@ -0,0 +1,11 @@
+public class usingoperators {
+    public static void main(String[] args) {
+     //   float a = 7/4*9/2;                                   
+       // System.out.println(a);                //here it shows 4 because when 4 is divided by 7 is shows 1 in integer and then it multiplies
+
+        float a = 7/4f*9/2f;
+        System.out.println(a);                 // here f is mentioned which shows    when 4 is divided by 7 it results in 1.75
+
+    }
+    
+}
