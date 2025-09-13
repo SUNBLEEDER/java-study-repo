8aefe778980807863b3d36def2c33603c64b29e2
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-13 18:52:31 +0530

    Add files via upload

---
+++ primitivedatatypes.java
@@ -0,0 +1,19 @@
+public class primitivedatatypes {
+
+    public static void main(String[] args){
+
+        int number =45555;  
+        float rishi = 99999f;
+        long raj = 796666666666l;
+        short karan = 4555;
+
+        System.out.println(number);
+        System.out.println(rishi);
+        System.out.println(raj);
+        System.out.println(karan);
+
+    
+
+    }
+    
+}
