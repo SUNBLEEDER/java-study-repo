f0bb1bf92903c9966fb78853f20bfe09a6a5312a
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-20 18:51:51 +0530

    Add files via upload

---
+++ twelfth.java
@@ -0,0 +1,14 @@
+public class twelfth {
+    public static void main(String[] args) {
+        
+        float marks[]= {45.6f,56.3f,77.7f};
+        float num = 56.3f;
+        for(float elements:marks){
+            if(num==elements){
+                System.out.println("The value is "  + elements);
+            }
+        }
+
+    }
+    
+}
