4781777891fcb87a2efb655bd99e7e6712d05e30
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-09-28 20:51:34 +0530

    Create random numbers.java

---
+++ random numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    
+
+    public static void main(String[] args) {
+        
+        // Use Math.random() method to generate random number
+        
+        System.out.println(" Random number1:"+ Math.random());
+        
+        System.out.println(" Random number2:"+ Math.random());
+        
+    
+    }
+}
