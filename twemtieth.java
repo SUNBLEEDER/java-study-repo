e3c993893bc7c3167213c1fb87722a1303ec8219
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-10-09 16:46:49 +0530

    Add files via upload

---
+++ twemtieth.java
@@ -0,0 +1,18 @@
+public class twemtieth {
+
+    static int rishi(int a,int b){
+        return  a+b;
+
+
+    }
+        static int rishi(int a,int b,int c ){
+        return  a+b+c;
+        }
+
+
+    public static void main(String[] args){
+        System.out.println("the sum of both the numbers is  :  "  + rishi( 4 , 5));
+           System.out.println("the sum of both the numbers is  :  "  + rishi( 4 , 5, 6));
+    }
+    
+}
