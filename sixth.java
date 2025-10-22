c24d841a5e47f22a896d9382b60457b41db95418
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-10-22 20:53:16 +0530

    Add files via upload

---
+++ sixth.java
@@ -0,0 +1,13 @@
+public class sixth {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        while(i<=999){
+            i++;
+            System.out.println(i);
+
+        }
+
+    }
+    
+}
