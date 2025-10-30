85446fbdce422aa7b07cf920bc346c3f55a71c2b
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-10-30 20:46:21 +0530

    Add files via upload

---
+++ seventeenth.java
@@ -0,0 +1,14 @@
+public class seventeenth {
+
+    static void change(int a){
+        a=98;
+    }
+
+
+    public static void main(String[] args){
+      int marks[]= {65,54,324,56,};
+      int x = 56;
+      change(x);
+      System.out.println(x);
+    }
+}
