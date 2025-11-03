66f60af3ec1eed6e5ec8472585b0109698db560e
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-11-03 23:22:20 +0530

    Add files via upload

---
+++ grade.java
@@ -0,0 +1,10 @@
+public class grade {
+
+  public static void main(String[] args){
+    
+    char grade = 'B';                                  //explanation of the code 
+    grade=(char)(grade+8);                             // here it is termed as char as it is a character and it adds upto 8 
+    System.out.println(grade);
+  }
+    
+}
