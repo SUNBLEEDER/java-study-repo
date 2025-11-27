5bfa84036daccb06d79ccf4fb70723fbefacd82d
Rishiraj Karan <rishirajkaran2006@gmail.com>
2025-11-27 20:20:47 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,9 @@
+public class uppercase {
+    public static void main(String[] args) {
+        
+        String name= "Rishiraj Karan";
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);
+    }
+    
+}
